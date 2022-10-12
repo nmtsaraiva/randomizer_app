fad277286a18b0b5ddaccf41e11fa295c4c6cc07
Nícolas Saraiva <114927446+nmtsaraiva@users.noreply.github.com>
2022-10-12 18:34:30 -0300

    added dialogs and the visilibilty widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:gap/gap.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -35,8 +37,20 @@ class FormNumbers extends StatefulWidget {
 }
 
 class _FormNumbersState extends State<FormNumbers> {
-  final TextEditingController _minimumController = TextEditingController(text: "0");
-  final TextEditingController _maximumController = TextEditingController(text: "1");
+  final TextEditingController _minimumController = TextEditingController();
+  final TextEditingController _maximumController = TextEditingController();
+
+  bool _isVisible = false;
+
+  var randomNumber = "";
+
+  Future<void> _showRandomized() async {
+    setState(()  {
+       randomNumber =  (int.parse(_minimumController.text) +  Random().nextInt((int.parse(_maximumController.text)) -
+          int.parse(_minimumController.text))).toString();
+       _isVisible = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,9 +61,11 @@ class _FormNumbersState extends State<FormNumbers> {
           padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
           child: TextFormField(
             keyboardType: TextInputType.number,
+            inputFormatters: [
+              FilteringTextInputFormatter.digitsOnly
+            ],
             decoration: const InputDecoration(
-              labelText: "Minimum",
-              hintText: "Minimum",
+              labelText: "Minimum", labelStyle: TextStyle(fontSize: 21),
             ),
             controller: _minimumController
           ),
@@ -58,9 +74,11 @@ class _FormNumbersState extends State<FormNumbers> {
           padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
           child: TextFormField(
             keyboardType: TextInputType.number,
+            inputFormatters: [
+              FilteringTextInputFormatter.digitsOnly
+            ],
             decoration: const InputDecoration(
-              labelText: "Maximum number",
-              hintText: "Maximum",
+              labelText: "Maximum", labelStyle: TextStyle(fontSize: 21),
             ),
             controller: _maximumController
           ),
@@ -72,42 +90,84 @@ class _FormNumbersState extends State<FormNumbers> {
             child: FloatingActionButton.extended(
               label: const Text("Generate"),
               backgroundColor: Colors.blue,
-              onPressed: () {
-                setState(() {
-                  _showNumberRandomized();
-                });
+              onPressed: () async {
+                if(_minimumController.text.isEmpty || _maximumController.text.isEmpty ) {
+                  showDialog(context: context, builder: (ctx) =>  AlertDialog(
+                    title: const Text("Error", style: TextStyle(fontSize: 21),),
+                    content: const Text("Enter a minimum and maximum number", style: TextStyle(fontSize: 21),),
+                    actions: [
+                      TextButton(
+                          onPressed: (){
+                            Navigator.of(ctx).pop();
+                            },
+                          child: Container(
+                            padding: const EdgeInsets.all(17),
+                            child: const Text("OK", style: TextStyle(fontSize: 21),),
+                          ))
+                    ],
+                  ));
+                } else if (int.parse(_minimumController.text) > int.parse(_maximumController.text)){
+                  showDialog(context: context, builder: (ctx) =>  AlertDialog(
+                    title: const Text("Error", style: TextStyle(fontSize: 21),),
+                    content: const Text("The minimum number cannot be greater than the maximum", style: TextStyle(fontSize: 21),),
+                    actions: [
+                      TextButton(
+                          onPressed: (){
+                            Navigator.of(ctx).pop();
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.all(17),
+                            child: const Text("OK", style: TextStyle(fontSize: 21),),
+                          ))
+                    ],
+                  ));
+                } else if(int.parse(_minimumController.text) == int.parse(_maximumController.text)){
+                  showDialog(context: context, builder: (ctx) =>  AlertDialog(
+                    title: const Text("Error", style: TextStyle(fontSize: 21),),
+                    content: const Text("The minimum number cannot be equal than the maximum", style: TextStyle(fontSize: 21),),
+                    actions: [
+                      TextButton(
+                          onPressed: (){
+                            Navigator.of(ctx).pop();
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.all(17),
+                            child: const Text("OK", style: TextStyle(fontSize: 21),),
+                          ))
+                    ],
+                  ));
+                } else {
+                  await _showRandomized();
+                }
               },
               tooltip: "Randomizer",
             ),
           ),
         ),
         const Gap(80),
-        const Center(
-          child: Text("Randomized number is: ", style: TextStyle(fontSize: 20),),
-        ),
-        const Gap(20),
-        _showNumberRandomized()
+        Visibility(
+            visible: _isVisible,
+            child: Column(
+              children: [
+                const Center(
+                  child: Text("Randomized number is: ", style: TextStyle(fontSize: 20),),
+                ),
+                const Gap(20),
+                Center(
+                  child: Text(
+                    randomNumber,
+                    style: const TextStyle(fontSize: 40),
+                  ),
+                )
+              ],
+            )
+        )
       ],
     );
   }
-   Widget _showNumberRandomized(){
-    if(_maximumController.text.isEmpty || _minimumController.text.isEmpty){
-      return const AlertDialog(
-        title: Text("Numbers must be entered"),
-      );
-    }else {
-      return
-        Center(
-          child: Center(child: Text(
-            (Random().nextInt(int.parse(_maximumController.text)) +
-                int.parse(_minimumController.text)).toString(),
-            style: const TextStyle(
-                fontSize: 40
-            ),)),
-        );
-    }
-  }
 }
+
+
 
 
 
